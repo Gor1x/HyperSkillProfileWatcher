5fbb84f4d347da1d06c299357487613af867f02c
Egor Postnikov <ea-post@yandex-team.ru>
2021-09-19 20:34:29 +0300

    initial

---
+++ app/src/main/java/com/postnikovegor/testproject/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.postnikovegor.testproject
+
+import android.content.Context
+import android.opengl.Visibility
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import com.google.gson.JsonParser
+import com.postnikovegor.testproject.databinding.ActivityMainBinding
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.InputStreamReader
+import java.net.URL
+import java.nio.charset.StandardCharsets
+import javax.net.ssl.HttpsURLConnection
+
+const val API_CONNECTION_TIMEOUT_TIME_MS = 4000
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            searchIdButton.setOnClickListener {
+                tryToShowUser(inputIdView.text.toString().toInt())
+                Toast.makeText(this@MainActivity, "Loading", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun tryToShowUser(id: Int) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val user = getUser(id)
+            val handler = Handler(Looper.getMainLooper())
+            handler.post {
+                loadUser(this@MainActivity, user)
+            }
+        }
+    }
+
+    private fun getUser(id: Int): User? {
+        val hyperSkillApi = URL("https://hyperskill.org/api/users/${id}?format=json")
+        val apiConnection: HttpsURLConnection = hyperSkillApi.openConnection() as HttpsURLConnection
+        apiConnection.connectTimeout = API_CONNECTION_TIMEOUT_TIME_MS
+        apiConnection.readTimeout = API_CONNECTION_TIMEOUT_TIME_MS
+        if (apiConnection.responseCode == 200) {
+            val responseBodyReader =
+                InputStreamReader(apiConnection.inputStream, StandardCharsets.UTF_8)
+            val currentUser = JsonParser.parseReader(responseBodyReader)
+                .asJsonObject.getAsJsonArray("users").get(0).asJsonObject
+            return Gson().fromJson(currentUser, User::class.java)
+        }
+        return null
+    }
+
+    private fun loadUser(context: Context, user: User?) {
+        if (user != null) {
+            binding.apply {
+                avatarView.visibility = View.VISIBLE
+                Picasso.get().load(user.avatar).into(avatarView)
+            }
+            Toast.makeText(context, "Done!", Toast.LENGTH_SHORT)
+                .show()
+        } else {
+            Toast.makeText(context, "Something went wrong, user not loaded", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/postnikovegor/testproject/User.kt
@@ -0,0 +1,43 @@
+package com.postnikovegor.testproject
+
+import com.google.gson.annotations.SerializedName
+
+data class User(
+    val id: Int,
+    val avatar: String,
+    @SerializedName("badge_title") val badgeTitle: String,
+    val bio: String,
+    val fullname: String,
+    val gamification: GamificationData,
+    @SerializedName("invitation_code") val invitationCode: String,
+    @SerializedName("comments_posted") val commentsPosted: CommentData,
+    val username: String,
+    @SerializedName("selected_tracks") val selectedTracks: List<Int>,
+    @SerializedName("completed_tracks") val completedTracks: List<Int>,
+    val country: String,
+    val languages: List<String>,
+    val experience: String,
+    @SerializedName("github_username") val githubUsername: String,
+    @SerializedName("linkedin_username") val linkedinUsername: String,
+    @SerializedName("twitter_username") val twitterUsername: String,
+    @SerializedName("reddit_username") val redditUsername: String,
+    @SerializedName("facebook_username") val facebookUsername: String,
+    @SerializedName("discord_id") val discordId: Int
+) {
+
+    data class CommentData(
+        val comment: Int,
+        val hint: Int,
+        @SerializedName("useful link") val usefulLink: Int,
+        val solutions: Int
+    )
+
+    data class GamificationData(
+        @SerializedName("active_days") val activeDays: Int,
+        @SerializedName("daily_step_completed_count") val dailyStepCompletedCount: Int,
+        @SerializedName("passed_problems") val passedProblems: Int,
+        @SerializedName("passed_projects") val passedProjects: Int,
+        @SerializedName("passed_topics") val passedTopics: Int,
+        @SerializedName("progress_updated_at") val progressUpdatedAt: String
+    )
+}
